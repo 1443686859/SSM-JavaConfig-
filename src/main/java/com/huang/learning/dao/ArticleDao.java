17d01ed1400982bbc1ef238579b142b33f8bc31a
1443686859 <1443686859@qq.com>
2019-10-24 13:13:28 +0800

    配置完善日志系统，slf4j log4j2

---
+++ src/main/java/com/huang/learning/dao/ArticleDao.java
@@ -0,0 +1,30 @@
+package com.huang.learning.dao;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.context.annotation.Bean;
+
+
+import java.util.List;
+
+
+public interface ArticleDao {
+	/**
+	 * 新增文章
+	 */
+	int addArticle(JSONObject jsonObject);
+
+	/**
+	 * 统计文章总数
+	 */
+	int countArticle(JSONObject jsonObject);
+
+	/**
+	 * 文章列表
+	 */
+	List<JSONObject> listArticle(JSONObject jsonObject);
+
+	/**
+	 * 更新文章
+	 */
+	int updateArticle(JSONObject jsonObject);
+}
